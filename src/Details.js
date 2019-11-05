4cd39c3ad0af2cde53a0c102a79d337048989d64
Gazouly <m.algazouly96@gmail.com>
2019-11-05 02:27:47 +0200

    Class Components

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,54 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
-const Details = props => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  state = { isLoading: true };
+
+  componentDidMount() {
+    pet
+      .animal(this.props.id)
+      .then(({ animal }) => {
+        this.setState({
+          isLoading: false,
+          name: animal.name,
+          type: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary
+        });
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    const {
+      name,
+      type,
+      location,
+      description,
+      breed,
+      media,
+      isLoading
+    } = this.state;
+    if (isLoading) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div className="details">
+          <Carousel media={media} />
+          <div>
+            <h1>{name}</h1>
+            <h2>{`${type} - ${breed}`}</h2>
+            <h2>{location}</h2>
+            <button>Adopt {name}</button>
+            <p>{description}</p>
+          </div>
+        </div>
+      );
+    }
+  }
+}
 
 export default Details;
